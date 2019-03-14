f05b02f3c409b39363f5f14e60762faadc67b8d4
nomnivore1 <48558380+nomnivore1@users.noreply.github.com>
2019-03-14 09:55:06 -0400

    Add files via upload

---
+++ thomas.cpp
@@ -0,0 +1,214 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+
+void elimination (double a[11], double b[11], double c[11], double z[11], double x[11])
+{
+	printf("elimination");
+	double m;
+	int i, n;
+for (n=2;n<=a[0];n++)
+	{
+	m=a[n]/b[n-1];
+	b[n]=b[n]-m*c[n-1];
+	z[n]=z[n]-m*z[n-1];
+	}
+	n=a[0];
+x[n]=z[n]/b[n];
+for (i=a[0]-1;i>=1;i--)
+	{
+	x[i]=(z[i]-c[i]*x[i+1])/b[i];
+	}
+}
+
+
+
+void write (double a1[11], double b1[11], double c1[11], double z1[11], double x1[11], double a2[11], double b2[11], double c2[11], double z2[11], double x2[11])
+{
+int n, i;
+	a1[1]=0;
+	n=a1[0];
+FILE * output;
+output = fopen ("thomasresults.txt", "w");	
+fprintf(output,"Matrix 1 components and solution:\r\n");
+fprintf(output,"A Array:\r\n");
+
+printf("Matrix 1 components and solution:\r\n");
+printf("A Array:\r\n");
+
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"%5.2lf\t",a1[y]);
+		}
+		
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");
+	fprintf(output,"B Array:\r\n");
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"%5.2lf\t",b1[y]);
+		
+		}
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");
+	fprintf(output,"c Array:\r\n");
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"%5.2lf\t",c1[y]);
+		
+		}
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");
+	fprintf(output,"D Array:\r\n");
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"%5.2lf\t",z1[y]);
+		}
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");
+	fprintf(output,"Solutions:\r\n");
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"X%i = %5.2lf\t",i, x1[y]);
+		}
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");	
+	
+//these loops will be repeated to write the results from matrix 2
+
+fprintf(output,"Matrix 2 components and solution:\r\n");
+fprintf(output,"A Array:\r\n");
+
+
+
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"%5.2lf\t",a2[y]);
+		
+		}
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");
+	fprintf(output,"B Array:\r\n");
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"%5.2lf\t",b2[y]);
+		
+		}
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");
+	fprintf(output,"c Array:\r\n");
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"%5.2lf\t",c2[y]);
+		
+		}
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");
+	fprintf(output,"D Array:\r\n");
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"%5.2lf\t",z2[y]);
+		}
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");
+	fprintf(output,"Solutions:\r\n");
+	for (int y=1;y <= n;y++)
+		{
+		
+		fprintf(output,"X%i = %5.2lf\t",i, x2[y]);
+		}	
+	fprintf(output,"\r\n");
+	fprintf(output,"\r\n");	
+	fclose(output);
+}
+
+
+
+int main ()
+{
+//initial arrays necessary for calculation
+double A1[11], B1[11], C1[11], Z1[11], X1[11], A2[11], B2[11], C2[11], Z2[11], X2[11], n;
+	int y;
+//arrays for saving input arrays for later output
+double A1x[11], B1x[11], C1x[11], Z1x[11], A2x[11], B2x[11], C2x[11], Z2x[11];
+printf("check");
+	FILE * input;
+input = fopen ("thomasdata.txt","r");
+
+printf("check");
+		//loop to read matrix 1 into arrays A1,  B1, and C1. 
+fscanf(input,"%lf\r\n",&n);
+printf("%lf",n);
+A1[0]= n;
+fscanf(input,"%lf %lf\r\n",B1[1], C1[1]);
+y=2;
+//BIG BLOCK OF CODE QUARANTINED FOR ALWAYS SEGFAULT CRIMES. REST OF CODE ONLY SEGFAULT SOMETIMES.
+/*for (int j=2;j<=n;j++)
+	{
+		for(int i=y-2;i>0;i--)
+		{
+		fscanf(input,"%lf");
+		}
+	fscanf(input,"%lf, %lf, %lf\r\n", &A1[y], &B1[y], &C1[y]);
+	A1x[y]=A1[y];
+	B1x[y]=B1[y];
+	C1x[y]=C1[y];
+	y++;
+	}
+	
+printf("check");
+			//loop to read matrix Z1
+	for (int y=1;y <= n;y++)
+		{
+		
+		fscanf(input,"%lf\r\n",&Z1[y]);
+		Z1x[y]=Z1[y];
+		}
+
+
+//the same loops will be used to read the second matrix
+fscanf(input,"%lf\r\n",&n);
+A2[0]= n;
+fscanf(input,"%lf %lf\r\n",B1[1], C1[1]);
+y=2;
+for (int j=2;j<=n;j++)
+	{
+		for(int i=y-2;i>0;i--)
+		{
+		fscanf(input,"%lf");
+		}
+	fscanf(input,"%lf, %lf, %lf\r\n", &A2[y], &B2[y], &C2[y]);
+	A2x[y]=A2[y];
+	B2x[y]=B2[y];
+	C2x[y]=C2[y];
+	}
+
+		
+	for (int y=1;y <= n;y++)
+		{
+		
+		fscanf(input,"%lf\r\n",&Z2[y]);
+		Z2x[y]=Z2[y];
+		}
+*/
+elimination(A1,B1,C1,Z1,X1);
+elimination(A2,B2,C2,Z2,X2);
+A1x[0]=A1[0];
+A2x[0]=A2[0];
+write(A1x,B1x,C1x,Z1x,X1,A2x,B2x,C2x,Z2x,X2);
+		
+fclose(input);
+return 0;
+}
